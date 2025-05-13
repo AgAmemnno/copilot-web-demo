2404700a451f2411250d68e0fe1a6cb880306f01
AgAmemnno <kaz380@hotmail.co.jp>
2025-05-14 06:46:23 +0900

    Add:Initial Auth Pat logic2 for development.

--- index.tsx
+++ index.tsx
@@ -2,23 +2,25 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
  */
+import { create_selector } from "./select";
+import {
+  FinishReason,
+  GenerateContentConfig,
+  GenerateContentParameters,
+  GoogleGenAI,
+  HarmBlockThreshold,
+  HarmCategory,
+  Part,
+  SafetySetting,
+} from '@google/genai';
 
 import { Octokit } from "https://esm.sh/@octokit/rest";
 
-// Personal Access Token (PAT) を環境変数から取得
-const token = window.globalThis.process.env.GITHUB_PAT;
 
-if (!token) {
-  throw new Error("GITHUB_PAT is not set. Please set your Personal Access Token in the environment variables.");
-}
 
-// Octokit インスタンスを作成
-const octokit = new Octokit({
-  auth: token,
-});
-
+const TARGET_GLOBAL_VAR_NAME = 'GITHUB_PAT';
 // 簡単なリクエスト: ユーザーのリポジトリ一覧を取得
-async function fetchRepositories() {
+async function fetchRepositories(octokit) {
   try {
     const response = await octokit.rest.repos.listForAuthenticatedUser();
     console.log("Repositories:");
@@ -33,7 +35,27 @@ async function fetchRepositories() {
 // メイン関数
 async function main() {
   console.log("Fetching repositories for the authenticated user...");
-  await fetchRepositories();
+  // Personal Access Token (PAT) を環境変数から取得
+  const token = window[TARGET_GLOBAL_VAR_NAME];
+
+  if (!token) {
+    throw new Error("GITHUB_PAT is not set. Please set your Personal Access Token in the environment variables.");
+  }
+    // Octokit インスタンスを作成
+  const octokit = new Octokit({
+        auth: token,
+  });
+  
+  await fetchRepositories(octokit);
 }
 
-main();
+document.addEventListener('DOMContentLoaded', function() {
+
+   
+    const selector = create_selector(TARGET_GLOBAL_VAR_NAME);
+    selector.open(main);
+
+});
+
+
+
